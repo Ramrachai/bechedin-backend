2da41f711a3e986daecc4963baf6ea65de03569b
ramrachai <ramrachaim@gmail.com>
2024-04-01 23:55:48 +0600

    user model added and configured nicely

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,68 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: false,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 4,
+    },
+    otp: {
+      type: Number,
+      expires: '5m',
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin', 'moderator'],
+      default: 'user',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+//hash password before saving users.
+userSchema.pre('save', async function (next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+//compare password
+userSchema.methods.comparePassword = async function (clientPassword, next) {
+  try {
+    const isMatch = await bcrypt.compare(clientPassword, this.password);
+    return isMatch;
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
